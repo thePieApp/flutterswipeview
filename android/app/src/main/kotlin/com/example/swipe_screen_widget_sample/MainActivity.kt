36b7ceb27bd6883ac536de1534ebaa7e6b2de3c8
Henry Yeung <henrywfyeung@gmail.com>
2021-05-23 11:05:03 +0800

    files

---
+++ android/app/src/main/kotlin/com/example/swipe_screen_widget_sample/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.swipe_screen_widget_sample
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/new_route/route2.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Route2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text(
+        'You Cancelled'
+      ),
+    );
+  }
+}
