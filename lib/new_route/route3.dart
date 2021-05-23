c84e1ac2112cc72dbe4cc2a591623fabed8874a6
Henry Yeung <henrywfyeung@gmail.com>
2021-05-23 10:59:37 +0800

    initial commit

---
+++ lib/new_route/route3.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Route3 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text(
+        'You liked with Message'
+      ),
+    );
+  }
+}
---
+++ lib/new_route/route4.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Route4 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text(
+        'You liked'
+      ),
+    );
+  }
+}
